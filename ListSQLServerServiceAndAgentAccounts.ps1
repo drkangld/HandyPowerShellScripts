fdb615e85f94c67520786527321b4f0b97f2a199
Cris Martinez <drkangld@rocketmail.com>
2022-04-20 11:24:39 -0500

    Update ListSQLServerServiceAndAgentAccounts.ps1
    Added lines to output service accounts to a file

--- ListSQLServerServiceAndAgentAccounts.ps1
+++ ListSQLServerServiceAndAgentAccounts.ps1
@@ -1,10 +1,12 @@
+#Get-WmiObject win32_service -ComputerName BRKAXHF10 | where {$_.name -eq 'MSSQLServer' -or $_.Name -eq 'SQLSERVERAGENT' } 
 cls
 $ComputerList = ''
 $ComputerList = Get-Content C:\Temp\ComputerList.txt
 $SQLServiceAccounts = @{}
 
 foreach ($Computer in $ComputerList) {
-  # I want separate hash pairs for SQL Server Service and Agent accounts because they can differ.
+  #$betty = $($(Get-WmiObject win32_service -ComputerName $Computer | where {$_.name -eq 'MSSQLServer'}).startname)
+  #Write-host "Computer is $Computer"
   $SQLServiceAccounts.add("$Computer-SQLServerService","$($(Get-WmiObject win32_service -ComputerName $Computer | where {$_.name -eq 'MSSQLServer'}).startname)")
   $SQLServiceAccounts.add("$Computer-SQLServerAgentService","$($(Get-WmiObject win32_service -ComputerName $Computer | where {$_.name -eq 'SQLSERVERAGENT'}).startname)")
 
@@ -12,11 +14,14 @@ foreach ($Computer in $ComputerList) {
   if ($SQLServiceAccounts."$Computer-SQLServerService" -eq $SQLServiceAccounts."$Computer-SQLServerAgentService") 
   {
    Write-host $SQLServiceAccounts."$Computer-SQLServerService" -ForegroundColor Green
+   Write-Output $SQLServiceAccounts."$Computer-SQLServerService" | Out-File c:\temp\SQLServiceAccounts.txt -Append
   }
   else
   {
   Write-host "SQL Server and Agent Services are different" 
   Write-host "$SQLServiceAccounts."$Computer-SQLServerService" $SQLServiceAccounts."$Computer-SQLServerAgentService"" -ForegroundColor Red
-    }
-  
+  write-output $SQLServiceAccounts."$Computer-SQLServerService" | Out-File c:\temp\SQLServiceAccounts.txt -Append
+  Write-Output $SQLServiceAccounts."$Computer-SQLServerAgentService" | Out-File c:\temp\SQLServiceAccounts.txt -Append
+   }
+ 
 }
