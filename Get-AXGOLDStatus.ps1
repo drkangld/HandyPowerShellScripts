91d99b13b58c0bd34833879898216a5010d30115
Cris Martinez <drkangld@rocketmail.com>
2022-07-19 17:02:01 -0500

    Example of windows message window with OK button
    Pulls scheduled task job status, timestamp, and AOS Service status and puts in a generic pop up window with an OK button.

---
+++ Get-AXGOLDStatus.ps1
@@ -0,0 +1,15 @@
+Set-ExecutionPolicy Bypass
+$timestamp = Get-Date
+$RefreshStatus = $($(Get-ScheduledTask -TaskName "Refresh AXGOLDBLD" -CimSession BRKAXGOLDBLD).state).tostring()
+$AOSStatus = $($(Get-Service -ComputerName brkaxgoldbld -name 'AOS60$01').Status).tostring()
+
+switch ($RefreshStatus)
+{
+  "Ready"     { $RefreshStatus = "Job is ready for next run at 6:30pm CT" }
+  "Disabled"  { $RefreshStatus = "Refresh job is DISABLED for maintenance" }
+  "Running"   { $RefreshStatus = "REFRESH IN PROGRES...AXGOLDBLD is not available at this time" }
+
+
+}
+
+[void] [System.Windows.MessageBox]::Show( "As of $timestamp`nCurrent Status of AXGOLD is: `n$RefreshStatus`nand the AOS is $AOSStatus", "Status of Nightly AXGOLDBLD Refresh", "OK", "Information" )
