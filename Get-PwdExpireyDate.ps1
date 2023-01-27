8ed6f03d9f2096edd732ef5828e3626d108bdf20
Cris Martinez <drkangld@rocketmail.com>
2023-01-27 16:13:09 -0600

    Create Get-PwdExpireyDate.ps1
    Because I'm lazy, when does my password expire.

---
+++ Get-PwdExpireyDate.ps1
@@ -0,0 +1,2 @@
+Get-ADUser -filter {Surname -eq "Martinez" -and Enabled -eq $True} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
+Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
