0d2460189ff115ea7aae9abae9a5d15e7c835d79
Cris Martinez <drkangld@rocketmail.com>
2023-11-14 07:00:06 -0600

    Create Get-JAMSJobChanges.ps1
    Since the audit logs only go back so far, we can look at all the jobs that have changed over a larger timeframe.  New jobs are considered a change.

---
+++ Get-JAMSJobChanges.ps1
@@ -0,0 +1,14 @@
+CLS
+$jobs = ''
+Import-Module JAMS
+New-PSDrive JD JAMS BRKJAMS1c -ErrorAction SilentlyContinue
+CD JD:\
+$jobs = Get-Childitem -Recurse -objecttype Job -ignorePredefined -FullObject
+cd c:\
+Remove-PSDrive JD
+#Filter for only changes in 2023
+$Report = $jobs | Select-Object JobName,MethodName,LastChange,QualifiedFolderName | where {$_.LastChange -like "*2023*"} 
+#Replace "MSDAX2012Job" with something more meaningful.
+foreach ($r in $Report) { if($r.MethodName -eq "MSDAX2012Job") {$r.MethodName="AX Batch Job"}}
+#Convert the object to .CSV and save to file.
+$Report | convertto-csv -NoTypeInformation  | out-file c:\temp\JAMS_Job_Changes_2023.csv 
